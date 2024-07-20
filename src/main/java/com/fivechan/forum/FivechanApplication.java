ae9b21272394498b98f8dcc1842657c973eb6e46
fhocus <fhocus.me@gmail.com>
2024-07-20 00:11:23 -0500

    start project

---
+++ src/main/java/com/fivechan/forum/FivechanApplication.java
@@ -0,0 +1,13 @@
+package com.fivechan.forum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FivechanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FivechanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fivechan/forum/FivechanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fivechan.forum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FivechanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
