33daa1e15f831b6f936198b2143e759737018007
fhocus <fhocus.me@gmail.com>
2024-07-30 22:02:57 -0500

    configurar la aplicación base

--- src/main/java/com/fivechan/forum/FivechanApplication.java
+++ src/main/java/com/fivechan/forum/FivechanApplication.java
@@ -3,7 +3,7 @@ package com.fivechan.forum;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.fivechan.forum")
 public class FivechanApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/fivechan/forum/apps/backend/controllers/topic/TopicController.java
+++ src/main/java/com/fivechan/forum/apps/backend/controllers/topic/TopicController.java
@@ -1,2 +1,28 @@
-package com.fivechan.forum.apps.backend.controllers.topic;public class TopicController {
+package com.fivechan.forum.apps.backend.controllers.topic;
+
+import com.fivechan.forum.context.topic.application.TopicService;
+import com.fivechan.forum.context.topic.domain.TopicDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/topic")
+public class TopicController {
+    private final TopicService topicService;
+
+    @Autowired
+    public TopicController(TopicService topicService) {
+        this.topicService = topicService;
+    }
+
+    @PostMapping
+    public  void createTopic(@RequestBody TopicDTO topic) {
+        UUID id = UUID.randomUUID();
+        this.topicService.createTopic(id, topic.getUserId(), topic.getTitle(), topic.getContent());
+    }
 }
